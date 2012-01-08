28caeca3a49bb021601f1a5612791e2861d9230d
Andreas Grech <agnt666@gmail.com>
2012-01-08 21:28:33 +0100

    Changed the functionality a bit; priority is now given to submitting the closest form, rather than clicking the closest button

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,4 @@
-var clickedEl, lastClosestButton, getClosestButton = function (el) {
+var clickedEl, getClosestButton = function (el) {
 	// TODO: Maybe you can add a limit to the number of levels it traverses for the button
         while ((el = el.parent()).length) {
             var buttons = el.find(":button, input[type=submit]"); // :button -> "input[type=button], button"
@@ -6,6 +6,23 @@ var clickedEl, lastClosestButton, getClosestButton = function (el) {
                 return buttons.eq(0);
             }
         }
+}, clickTheButton = function (button) {
+	if (button) {
+		button.trigger('click');
+		console.log('Clicking on the closest button');
+	}
+}, submitClosestForm = function(box) { 
+	var closestForm;
+	if (box) {
+		closestForm = box.closest("form");
+		if (!closestForm.length) { // Didn't find any forms
+			return;
+		}
+
+		closestForm.submit();
+		console.log('Submiting the closest form');
+		return true;
+	}
 };
 
 document.addEventListener("mousedown", function (ev) {
@@ -17,14 +34,11 @@ document.addEventListener("mousedown", function (ev) {
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     var box = $(clickedEl);
 
-    if (request.action === "findClosestButton") {
-        lastClosestButton = getClosestButton(box);
-        sendResponse(typeof lastClosestButton !== "undefined");
-        return;
-    }
+    if (request.action === "flushAndSubmit") {
+        box.val(request.data); // paste the data in the input box
 
-    if (request.action === "insertBufferContents") {
-        box.val(request.data);
-        lastClosestButton && lastClosestButton.trigger('click');
+	if (!submitClosestForm(box)) { // If we didn't find a form, try clicking on the closest button
+		clickTheButton(getClosestButton(box));
+	}
     }
 });
--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 var buffer = document.getElementById('buffer');
 
 chrome.contextMenus.create({ type: "normal", title: "Paste and Go", contexts: ["editable"], onclick: function (info, tab) {
-        chrome.tabs.sendRequest(tab.id, { action: "findClosestButton" }, function (buttonIsFound) { // tell the content script to find the closest button that we can click on
-	    if (!buttonIsFound) { // if no button is found, don't do anything else
-	        return;
-	    }
-
             buffer.value = '';
 	    buffer.select();
             document.execCommand('paste');
-            chrome.tabs.sendRequest(tab.id, { action: "insertBufferContents", data: buffer.value});
-        });
+
+            chrome.tabs.sendRequest(tab.id, { action: "flushAndSubmit", data: buffer.value});
     }
 });
