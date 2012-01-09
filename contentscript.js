a4c0835e1fd76206b781462e7166a29ad096125d
Andreas Grech <agnt666@gmail.com>
2012-01-09 22:58:28 +0100

    Instead of reversing the buttons in the array, I've now changed the logic of the algorithm to simply return the final element in the array i.e. O(1) rather than O(n)

--- contentscript.js
+++ contentscript.js
@@ -2,10 +2,9 @@ var clickedEl, getClosestButton = function (el) {
 	// TODO: Maybe you can add a limit to the number of levels it traverses for the button
         while ((el = el.parent()).length) {
             var buttons = el.find(":button, input[type=submit]"); // :button -> "input[type=button], button"
-	    buttons = Array.prototype.reverse.call(buttons);
 
             if (buttons.length) {
-                return buttons.eq(0);
+                return buttons.eq(buttons.length - 1);
             }
         }
 	// Didn't find any buttons
